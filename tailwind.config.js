1b85bc313207d147eccc53df2fedcc9855b46e3b
aditya <adityakadali00@gmail.com>
2022-09-04 23:43:42 +0530

    added custom colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          500: "#26C0AB",
+          900: "#00494D",
+        },
+        secondary: {
+          50: "#FFFFFF",
+          100: "#F4FAFA",
+          200: "#C5E4E7",
+          400: "#7F9C9F",
+          600: "#5E7A7D",
+        },
+      },
+      fontFamily: {
+        spacemono: "'Space Mono', monospace",
+      },
+    },
   },
   plugins: [],
 };
